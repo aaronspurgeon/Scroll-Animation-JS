f9db1d1326bdef3602086ea8fdce66ecc4f4f792
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-07-19 22:05:33 -0400

    added animations to the paper plane using gsap and scrollmagic

--- app.js
+++ app.js
@@ -9,16 +9,27 @@ const flightPath = {
         {x: 350, y: -50},
         {x: 600, y: 100},
         {x: 800, y: 0},
-        {x: 500, y: -250},
-        {x: window.innerWidth, y: 150}
+        {x: window.innerWidth, y: -250}
     ]
 }
 
 const tween = new TimelineLite();
 
 tween.add(
-    TweenLite.to('.paper-plane', 7, {
+    TweenLite.to('.paper-plane', 1, {
         bezier: flightPath,
         ease: Power1.easeInOut
     })
-)
+);
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.animation',
+    duration: 1000,
+    triggerHook: 0
+})
+.setTween(tween)
+.addIndicators()
+.setPin('.animation')
+.addTo(controller);
